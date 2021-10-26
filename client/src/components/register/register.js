51e12e821a411b366776d7ec84f170650a565a32
Hiromi Acosta <mafequevedo1@gmail.com>
2021-10-26 16:15:14 -0500

    modificaciones back y rutas

--- client/src/components/register/register.js
+++ client/src/components/register/register.js
@@ -1,54 +1,81 @@
-import React, { useState } from "react"
-import "./register.css"
-import axios from "axios"
-import { useHistory } from "react-router-dom"
+import React, { useState } from "react";
+import "./register.css";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
 
 const Register = () => {
+    const history = useHistory();
 
-    const history = useHistory()
-
-    const [ user, setUser] = useState({
+    const [user, setUser] = useState({
         name: "",
-        email:"",
-        password:"",
-        reEnterPassword: ""
-    })
+        email: "",
+        password: "",
+        reEnterPassword: "",
+    });
 
-    const handleChange = e => {
-        const { name, value } = e.target
+    const handleChange = (e) => {
+        const { name, value } = e.target;
         setUser({
             ...user,
-            [name]: value
-        })
-    }
+            [name]: value,
+        });
+    };
 
     const register = () => {
-        const { name, email, password, reEnterPassword } = user
-        if( name && email && password && (password === reEnterPassword)){
-            axios.post("http://localhost:9002/register", user)
-            .then( res => {
-                alert(res.data.message)
-                history.push("/login")
-            })
+        const { name, email, password, reEnterPassword } = user;
+        if (name && email && password && password === reEnterPassword) {
+            axios
+                .post("https://intopcol-back.herokuapp.com/register", user)
+                .then((res) => {
+                    alert(res.data.message);
+                    history.push("/login");
+                });
         } else {
-            alert("invlid input")
+            alert("invlid input");
         }
-        
-    }
+    };
 
     return (
         <div className="register">
             {console.log("User", user)}
             <h1>Registro</h1>
-            <input type="text" name="name" value={user.name} placeholder="Nombre" onChange={ handleChange }></input>
-            <input type="text" name="email" value={user.email} placeholder="Email" onChange={ handleChange }></input>
-            <input type="password" name="password" value={user.password} placeholder="Contraseña" onChange={ handleChange }></input>
-            <input type="password" name="reEnterPassword" value={user.reEnterPassword} placeholder="Repetir Contraseña" onChange={ handleChange }></input>
-            <div className="button" onClick={register} >Registrarse</div>
+            <input
+                type="text"
+                name="name"
+                value={user.name}
+                placeholder="Nombre"
+                onChange={handleChange}
+            ></input>
+            <input
+                type="text"
+                name="email"
+                value={user.email}
+                placeholder="Email"
+                onChange={handleChange}
+            ></input>
+            <input
+                type="password"
+                name="password"
+                value={user.password}
+                placeholder="Contraseña"
+                onChange={handleChange}
+            ></input>
+            <input
+                type="password"
+                name="reEnterPassword"
+                value={user.reEnterPassword}
+                placeholder="Repetir Contraseña"
+                onChange={handleChange}
+            ></input>
+            <div className="button" onClick={register}>
+                Registrarse
+            </div>
             <div>or</div>
-            <div className="button" onClick={() => history.push("/login")}>Iniciar Sesion</div>
+            <div className="button" onClick={() => history.push("/login")}>
+                Iniciar Sesion
+            </div>
         </div>
-    )
-}
+    );
+};
 
-export default Register
+export default Register;
