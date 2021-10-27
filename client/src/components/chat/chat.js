e82d7939d6abab361ff5e52a0f1dc878c1d76b4e
DESKTOP-LO51G0C\sergi <verasergio700@gmail.com>
2021-10-26 21:26:04 -0500

    se crea el chat

--- client/src/components/chat/chat.js
+++ client/src/components/chat/chat.js
@@ -1,34 +1,69 @@
-import React from "react"
+import TextField from "@material-ui/core/TextField"
+import React, { useEffect, useRef, useState } from "react"
+import io from "socket.io-client"
 import "./chat.css"
-import { useHistory } from "react-router-dom"
 
-const Chat = ({setLoginUser}) => {
+function Chat() {
+	const [ state, setState ] = useState({ message: "" })
+	const [ chat, setChat ] = useState([])
 
-    const history = useHistory()
+	const socketRef = useRef()
 
-    return (
-        <div class="chat-container">
-            <header class="chat-header">
-                <h1><i class="fas fa-smile"></i> Chat</h1>
-                <div id="leave-btn"  onClick={() => history.push("/")}  class="btn">Salir de la sala</div>
-            </header>
-            <main class="chat-main">
-                <div class="chat-sidebar">
-                <h3><i class="fas fa-comments"></i> Nombre de la sala:</h3>
-                <h2 id="room-name"></h2>
-                <h3><i class="fas fa-users"></i> Usuarios</h3>
-                <ul id="users"></ul>
-                </div>
-                <div class="chat-messages"></div>
-            </main>
-            <div class="chat-form-container">
-                <form id="chat-form">
-                <input id="msg" type="text" placeholder="Ingrese Mensaje" required autocomplete="off" />
-                <button class="btn">Enviar</button>
-                </form>
-            </div>
-        </div>
-    )
+	useEffect(
+		() => {
+			socketRef.current = io.connect("http://localhost:9002")
+			socketRef.current.on("message", ({  message }) => {
+				setChat([ ...chat, {  message } ])
+			})
+			return () => socketRef.current.disconnect()
+		},
+		[ chat ]
+	)
+
+    const onTextChange = (e) => {
+		setState({ ...state, [e.target.name]: e.target.value })
+	}
+
+	const onMessageSubmit = (e) => {
+		const {  message } = state
+		socketRef.current.emit("message", {  message })
+		e.preventDefault()
+		setState({ message: "" })
+	}
+
+	const renderChat = () => {
+		return chat.map(({  message }, index) => (
+			<div key={index}>
+				<h3>
+					mensaje: <span>{message}</span>
+				</h3>
+			</div>
+		))
+	}
+
+	return (
+		<div className="card">
+			<form onSubmit={onMessageSubmit}>
+				<h1>Mensaje</h1>
+				<div>
+					<TextField
+						name="message"
+						onChange={(e) => onTextChange(e)}
+						value={state.message}
+						id="outlined-multiline-static"
+						variant="outlined"
+						label="Mensaje"
+					/>
+				</div>
+				<button className="btn btn-primary">Enviar Mensaje</button>
+			</form>
+			<div className="render-chat">
+				<h1>Chat</h1>
+				{renderChat()}
+			</div>
+		</div>
+	)
 }
+
 
 export default Chat
